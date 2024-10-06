c18e2d1eeccb9e927380815c2a42e678b749c97d
fiartaks <fiartaks@gmail.com>
2024-10-06 17:52:16 +0300

    first commit

---
+++ ebruSanati/src/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './css/Header.css';
+
+function Header() {
+  return (
+    <div className='header'>
+        <div className='title'>EBRU SANATI ESERLERİM</div>
+
+    </div>
+  )
+}
+
+export default Header
---
+++ ebruSanati/src/data.jsx
@@ -0,0 +1,107 @@
+import Ebru1 from './images/ebru1.jpg';
+import Ebru2 from './images/ebru2.jpg';
+import Ebru3 from './images/ebru3.jpg';
+import Ebru4 from './images/ebru4.jpg';
+import Ebru5 from './images/ebru5.jpg';
+import Ebru6 from './images/ebru6.jpg';
+import Ebru7 from './images/ebru7.jpg';
+import Ebru8 from './images/ebru8.jpg';
+import Ebru9 from './images/ebru9.jpg';
+import Ebru10 from './images/ebru10.jpg';
+
+
+
+
+export const courses =[
+    {
+        id:253,
+        title:"Battal Ebru",
+        description: "Battal ebru serbest,geniş ve soyut desenlerle yapılır. Bu tür ebru, rastgele dökülen ve serbest bırakılan boyaların yüzeyde yarattığı desenlerle tanınır.",
+        price: 1400,
+        link: "https://fiartaks.netlify.app/",
+        image : Ebru1
+
+    },
+    {
+        id:254,
+        title:"Karanfil",
+        description: "Ebru sanatında karanfil çiçeği, boyaların su yüzeyine damlatılması ve özel aletlerle şekillendirilmesiyle oluşturulan geleneksel bir motifidir.",
+        price: 2500,
+        link: "https://fiartaks.netlify.app/",
+        image : Ebru2
+
+    },
+    {
+        id:1244,
+        title:"Battal Ebru",
+        description: "Battal ebru, serbest, geniş ve soyut desenlerle yapılır. Bu tür ebru, rastgele dökülen ve serbest bırakılan boyaların yüzeyde yarattığı desenlerle tanınır.",
+        price: 3000,
+        link: "https://fiartaks.netlify.app/",
+        image : Ebru3
+
+    },{
+        id:1244,
+        title:"Gelincik",
+        description: "Geleneksel klasik Ebru sanatında gelincik çiçeği, su yüzeyindeki boyaların dikkatle şekillendirilmesiyle elde edilen zarif ve doğal bir motiftir. ",
+        price: 1200,
+        link: "https://fiartaks.netlify.app/",
+        image : Ebru4
+
+    },
+    {
+        id:253,
+        title:"Lale",
+        description: "Geleneksel klasik ebru sanatında lale çiçeği, ince uçlu fırça veya bizle su üzerindeki boyaya şekil verilerek oluşturulan klasik bir motiftir.",
+        price: 1200,
+        link: "https://fiartaks.netlify.app/",
+        image : Ebru5
+
+    },
+    {
+        id:253,
+        title:"Lale Demeti",
+        description: "Ebru sanatında lale demeti, birden fazla lalenin birlikte kompoze edilmesiyle su yüzeyinde zarif ve estetik bir motif oluşturulur.",
+        price: 2200,
+        link: "https://fiartaks.netlify.app/",
+        image : Ebru6
+
+    },
+    {
+        id:253,
+        title:"Kasımpatı",
+        description: "Ebru sanatında kasımpatı, su yüzeyine damlatılan boyaların ince detaylarla şekillendirildiği, çok yapraklı ve detaylı bir çiçek motifidir.",
+        price: 1200,
+        link: "https://fiartaks.netlify.app/",
+        image : Ebru7
+
+    },
+    {
+        id:253,
+        title:"Tezyin",
+        description: "Ebru sanatında tezyin, desenlerin ve motiflerin su üzerine estetik biçimde düzenlenmesiyle oluşturulan süsleme ve bezeme sanatıdır.",
+        price: 3500,
+        link: "https://fiartaks.netlify.app/",
+        image : Ebru8
+
+    },
+    {
+        id:253,
+        title:"Kaligrafi",
+        description: "Ebru sanatında kaligrafi, renkli ebru zemin üzerine hat sanatıyla yazılmış yazılar eklenerek görsel estetik ve anlam zenginliği katılır.",
+        price: 4000,
+        link: "https://fiartaks.netlify.app/",
+        image : Ebru9
+
+    },
+    {
+        id:253,
+        title:"Gelgit Ebru",
+        description: "Battal ebru, serbest, geniş ve soyut desenlerle yapılır. Bu tür ebru, rastgele dökülen ve serbest bırakılan boyaların yüzeyde yarattığı desenlerle tanınır.",
+        price: 1000,
+        link: "https://fiartaks.netlify.app/",
+        image : Ebru10
+
+    },
+    
+
+]
